872eaa8b0eddce726712e07a311275690543ec5b
Burke Cates <burke.cates@gmail.com>
2025-08-31 20:43:00 -0700

    add home configs for freddie-kane and juicy-j

---
+++ homes/x86_64-linux/burke@juicy-j/ai.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }:
+{
+  home.sessionVariables = {
+    OLLAMA_API_BASE = "http://127.0.0.1:11434";
+  };
+}
---
+++ homes/x86_64-linux/burke@juicy-j/default.nix
@@ -0,0 +1,34 @@
+{ ... }:
+{
+  imports = [ ./ai.nix ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "burke";
+  home.homeDirectory = "/home/burke";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  #services.home-manager.autoExpire
+  programs.git = {
+    userName = "Burke Cates";
+    userEmail = "burke.cates@gmail.com";
+  };
+
+  dconf.settings = {
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
+    };
+  };
+}
