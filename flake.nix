1d8cc78de7c01f6718e13be9362ccde03520eab6
Burke Cates <burke.cates@gmail.com>
2026-05-31 00:43:01 -0700

    hydraJobs for my nixosSystems!

--- flake.nix
+++ flake.nix
@@ -71,58 +71,68 @@
 
   outputs =
     inputs:
-    inputs.snowfall-lib.mkFlake {
-      # You must provide our flake inputs to Snowfall Lib.
-      inherit inputs;
+    let
+      flake = inputs.snowfall-lib.mkFlake {
+        # You must provide our flake inputs to Snowfall Lib.
+        inherit inputs;
 
-      # The `src` must be the root of the flake. See configuration
-      # in the next section for information on how you can move your
-      # Nix files to a separate directory.
-      src = ./.;
+        # The `src` must be the root of the flake. See configuration
+        # in the next section for information on how you can move your
+        # Nix files to a separate directory.
+        src = ./.;
 
-      snowfall.namespace = "t11s";
+        snowfall.namespace = "t11s";
 
-      overlays = with inputs; [
-        (final: _prev: {
-          unstable = import inputs.unstable {
-            inherit (final) config;
-            localSystem = final.stdenv.hostPlatform;
-          };
-        })
-        systemctl-toggle.overlays.default
-        niri.overlays.niri
-        tidalcycles.overlays.tidal
-      ];
-      alias = {
-        shells.default = "nix-dev";
+        overlays = with inputs; [
+          (final: _prev: {
+            unstable = import inputs.unstable {
+              inherit (final) config;
+              localSystem = final.stdenv.hostPlatform;
+            };
+          })
+          systemctl-toggle.overlays.default
+          niri.overlays.niri
+          tidalcycles.overlays.tidal
+        ];
+        alias = {
+          shells.default = "nix-dev";
+        };
+
+        homes.modules = with inputs; [
+          catppuccin.homeModules.catppuccin
+          caelestia-niri.homeManagerModules.default
+          #niri.homeModules.config
+        ];
+        homes.users."burke@freddie-kane".modules = with inputs; [
+          system76-scheduler-niri.homeModules.default
+        ];
+
+        systems.modules.nixos = with inputs; [
+          catppuccin.nixosModules.catppuccin
+          determinate.nixosModules.default
+          lanzaboote.nixosModules.lanzaboote
+          niri.nixosModules.niri
+          programsdb.nixosModules.programs-sqlite
+          agenix.nixosModules.default
+        ];
+        systems.hosts.lofty305-wsl-nixos.modules = with inputs; [
+          nixos-wsl.nixosModules.default
+        ];
+
+        outputs-builder = channels: { formatter = channels.nixpkgs.nixfmt-tree; };
+
+        channels-config = {
+          # Allow unfree packages.
+          allowUnfree = true;
+        };
       };
-
-      homes.modules = with inputs; [
-        catppuccin.homeModules.catppuccin
-        caelestia-niri.homeManagerModules.default
-        #niri.homeModules.config
-      ];
-      homes.users."burke@freddie-kane".modules = with inputs; [
-        system76-scheduler-niri.homeModules.default
-      ];
-
-      systems.modules.nixos = with inputs; [
-        catppuccin.nixosModules.catppuccin
-        determinate.nixosModules.default
-        lanzaboote.nixosModules.lanzaboote
-        niri.nixosModules.niri
-        programsdb.nixosModules.programs-sqlite
-        agenix.nixosModules.default
-      ];
-      systems.hosts.lofty305-wsl-nixos.modules = with inputs; [
-        nixos-wsl.nixosModules.default
-      ];
-
-      outputs-builder = channels: { formatter = channels.nixpkgs.nixfmt-tree; };
-
-      channels-config = {
-        # Allow unfree packages.
-        allowUnfree = true;
+    in
+    flake
+    // {
+      hydraJobs = {
+        nixos = inputs.nixpkgs.lib.mapAttrs (
+          _: cfg: cfg.config.system.build.toplevel
+        ) flake.nixosConfigurations;
       };
     };
 }
